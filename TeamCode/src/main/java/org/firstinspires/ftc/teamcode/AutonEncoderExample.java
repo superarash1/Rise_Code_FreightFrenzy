6ab0f817d4f0fb288dee3c8032bb13e76c1016b7
superarash1 <72105024+superarash1@users.noreply.github.com>
2021-11-16 22:27:49 -0500

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonEncoderExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonEncoderExample.java
@@ -12,18 +12,31 @@ public class AutonEncoderExample extends LinearOpMode {
     Hardware TIseBot = new Hardware();
     private ElapsedTime runtime = new ElapsedTime();
 
-    // FineTune
+    // Constants to find the amount of encoder ticks per MM
     static final double COUNTS_PER_MOTOR_REV = 537.7;
-    static final double DRIVE_GEAR_REDUCTION = 0.66; //Could be wrong; if things break, look at this
-    static final double WHEEL_DIAMETER_MM = 96.0;
+    static final double DRIVE_GEAR_REDUCTION = 1.35;
+    static final double WHEEL_DIAMETER_CM = 9.60;
 
-    //static final double COUNTS_PER_MM = ((((1+(46/17))) * (1+(46/11))) * 28); // Potentially use goBilda provided code
-    static final double COUNTS_PER_MM = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_MM * 3.1415);
+    // Finds the amount of encoder ticks per MM
+    static final double COUNTS_PER_CM = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_CM * 3.1415);
 
-    // Add P-Drive
-    static final double DRIVE_SPEED_1 = 0.6;
 
-    static final double distance1  = 1000;
+    // These are the distances each motor will go (negation means opposite direction, which is why each motor is separately defined)
+
+    static final double fLeft1  = 100;
+    static final double fRight1  = 100;
+    static final double bRight1  = 100;
+    static final double bLeft1  = 100;
+
+    static final double fLeft2  = -57.5;
+    static final double fRight2  = 57.5;
+    static final double bRight2  = 57.5;
+    static final double bLeft2  = -57.5;
+
+    static final double fLeft3  = 100;
+    static final double fRight3  = -100;
+    static final double bRight3  = 100;
+    static final double bLeft3  = -100;
 
     // Creating an array for the wheels (makes it more organized)
     DcMotor[] wheels = new DcMotor[4];
@@ -47,7 +60,7 @@ public class AutonEncoderExample extends LinearOpMode {
             wheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         }
 
-
+        // Telemetry to show initial encoder position
         telemetry.addData("Path0",  "Starting at %7d :%7d :%7d :%7d", TIseBot.frontLeftMotor.getCurrentPosition(), TIseBot.frontRightMotor.getCurrentPosition(), TIseBot.backRightMotor.getCurrentPosition(), TIseBot.backLeftMotor.getCurrentPosition());
         telemetry.update();
 
@@ -55,20 +68,33 @@ public class AutonEncoderExample extends LinearOpMode {
 
         waitForStart();
 
-        encoderDrive(DRIVE_SPEED_1,  distance1, 5.0);  // S1: Forward 300 Millimeters with 5 Sec timeout
+        // Robot drives 100 cm forward (roughly)
+        encoderDrive(0.6 , fLeft1, fRight1, bRight1, bLeft1, 5.0);
+        sleep(250);
 
+        // Robot Turns roughly 90 degrees tho everything is measured in mm
+        encoderDrive(0.6, fLeft2, fRight2, bRight2, bLeft2, 5.0);
+        sleep(250);
+
+        // Robot strafes back to start
+        encoderDrive(0.6, fLeft3, fRight3, bRight3, bLeft3, 5.0);
+        sleep(250);
     }
-    public void encoderDrive(double speed, double distanceMM, double timeoutS) {
+
+    public void encoderDrive(double speed, double fLeftMM, double fRightMM,double bRightMM,double bLeftMM, double timeoutS) {
 
         // Creates an array for the target positions for each of your motors
         int []newWheelTarget = new int[4];
 
         if (opModeIsActive()) {
 
-            for (int i = 0; i < 4; i++) {
-                // Defines the wheel target
-                newWheelTarget[i] = wheels[0].getCurrentPosition() + (int)(distanceMM * COUNTS_PER_MM);
+            // Defines the target position for your motor
+            newWheelTarget[0] = wheels[0].getCurrentPosition() + (int)(fLeftMM * COUNTS_PER_CM);
+            newWheelTarget[1] = wheels[1].getCurrentPosition() + (int)(fRightMM * COUNTS_PER_CM);
+            newWheelTarget[2] = wheels[2].getCurrentPosition() + (int)(bRightMM * COUNTS_PER_CM);
+            newWheelTarget[3] = wheels[3].getCurrentPosition() + (int)(bLeftMM * COUNTS_PER_CM);
 
+            for (int i = 0; i < 4; i++) {
                 // Sets the target position for the motors
                 wheels[i].setTargetPosition(newWheelTarget[i]);
 
@@ -87,24 +113,20 @@ public class AutonEncoderExample extends LinearOpMode {
 
             // Will keep looping until one of the of the motors reach the target position
             while (opModeIsActive() && (runtime.seconds() < timeoutS) && (TIseBot.frontLeftMotor.isBusy() && TIseBot.frontRightMotor.isBusy() && TIseBot.backRightMotor.isBusy() && TIseBot.backLeftMotor.isBusy())) {
-                // Display it for the driver.
+                // Displays the target position and current position in telemetry
                 telemetry.addData("Path1",  "Running to %7d :%7d :%7d :%7d", newWheelTarget[0],  newWheelTarget[1], newWheelTarget[2], newWheelTarget[3]);
                 telemetry.addData("Path2",  "Running at %7d :%7d :%7d :%7d", TIseBot.frontLeftMotor.getCurrentPosition(), TIseBot.frontRightMotor.getCurrentPosition(), TIseBot.backRightMotor.getCurrentPosition(), TIseBot.backLeftMotor.getCurrentPosition());
                 telemetry.update();
             }
 
-            // Stops all motion;
-            TIseBot.frontLeftMotor.setPower(0);
-            TIseBot.frontRightMotor.setPower(0);
-            TIseBot.backRightMotor.setPower(0);
-            TIseBot.backLeftMotor.setPower(0);
+            for (DcMotor wheel : wheels){
+                // Stops motors after motors have reached target position
+                wheel.setPower(0);
 
-
-            // Turn off RUN_TO_POSITION
-            TIseBot.frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            TIseBot.frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            TIseBot.backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            TIseBot.backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                // Resets encoders
+                wheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                wheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
 
             //  sleep(50);   // optional pause after each move
         }
