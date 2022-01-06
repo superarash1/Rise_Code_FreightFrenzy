5d3eab92c2a7d24d86f534aacf54f5c4e88c311c
superarash1 <72105024+superarash1@users.noreply.github.com>
2022-01-05 21:52:57 -0500

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_Practice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_Practice.java
@@ -21,8 +21,6 @@ public class PID_Practice extends LinearOpMode {
     // Finds the amount of encoder ticks per CM
     static final double COUNTS_PER_CM = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_CM * 3.1415);
 
-    static final double distance = 75;
-
     DcMotor[] wheels = new DcMotor[4];
 
     @Override
@@ -47,17 +45,11 @@ public class PID_Practice extends LinearOpMode {
 
         waitForStart();
 
-        PIDDrive(100, 1);
+        PIDDrive(10, 1);
         sleep(100);
 
-        PIDTurn(59, 1);
+        PIDDrive(-10, 1);
         sleep(100);
-
-        PIDStrafe(-116, 1);
-        sleep(100);
-
-        PIDTurn(-59, 1);
-
 
     }
 
@@ -75,19 +67,27 @@ public class PID_Practice extends LinearOpMode {
 
             double[] P = new double[4];
 
-            double kp = 1;
-            double kd = 0.000007; // Constant of derivation
-            double ki = 0.00002;
+            double kp = 1.35;
+            double kd = 0.0002; // Constant of derivation
+            double ki = 0.000005;
 
-            double dt = 20; //Delta time = 20 ms/cycle
+            double kStatic = 0;
+            double kV = 0;
+
+            double referenceVelocity = 0;
+
+            double dt = 50; //Delta time = 20 ms/cycle
             double dtS = dt/1000;
 
             double[] error = new double[4];
             double[] previousError = new double[4];
-            double[] power = new double[4];
 
             double[] area = new double[4];
             double[] previousArea = new double[4];
+
+            double[] PID = new double[4];
+            double[] power = new double[4];
+
 
             error[0] = tolerance + 1;
 
@@ -96,10 +96,12 @@ public class PID_Practice extends LinearOpMode {
                 telemetry.addData("Path1",  "Running to %7d :%7d :%7d :%7d", newWheelTarget[0], newWheelTarget[1], newWheelTarget[2], newWheelTarget[3]);
                 telemetry.addData("Path2",  "Running at %7d :%7d :%7d :%7d", wheels[0].getCurrentPosition(), wheels[1].getCurrentPosition(), wheels[2].getCurrentPosition(), wheels[3].getCurrentPosition());
 
-
-                telemetry.addData("DistanceMM: ", (int)(distanceCM * COUNTS_PER_CM));
+                telemetry.addData("DistanceCM: ", (int)(distanceCM * COUNTS_PER_CM));
 
                 telemetry.addData("power: ", power[0]);
+
+                telemetry.addData("Friction FeedForward:", kStatic * Math.signum(PID[0]));
+                telemetry.addData("Velocity FeedForward:", kV * (referenceVelocity * Math.signum(PID[0])));
 
                 telemetry.addData("Proportion:", P[0]);
                 telemetry.addData("Derivative:", kd * ((error[0] - previousError[0]) / dtS));
@@ -141,11 +143,16 @@ public class PID_Practice extends LinearOpMode {
                 area[1] = error[1] * dtS + previousArea[1];
                 area[2] = error[2] * dtS + previousArea[2];
                 area[3] = error[3] * dtS + previousArea[3];
+                
+                PID[0] = kp * P[0] + kd * ((error[0] - previousError[0]) / dtS) + (ki * area[0]);
+                PID[1] = kp * P[1] + kd * ((error[1] - previousError[1]) / dtS) + (ki * area[1]);
+                PID[2] = kp * P[2] + kd * ((error[2] - previousError[2]) / dtS) + (ki * area[2]);
+                PID[3] = kp * P[3] + kd * ((error[3] - previousError[3]) / dtS) + (ki * area[3]);
 
-                power[0] = kp * P[0] + kd * ((error[0] - previousError[0]) / dtS) + (ki * area[0]);
-                power[1] = kp * P[1] + kd * ((error[1] - previousError[1]) / dtS) + (ki * area[1]);
-                power[2] = kp * P[2] + kd * ((error[2] - previousError[2]) / dtS) + (ki * area[2]);
-                power[3] = kp * P[3] + kd * ((error[3] - previousError[3]) / dtS) + (ki * area[3]);
+                power[0] = PID[0] + kStatic * Math.signum(PID[0]) + kV * (referenceVelocity * Math.signum(PID[0]));
+                power[1] = PID[1] + kStatic * Math.signum(PID[1]) + kV * (referenceVelocity * Math.signum(PID[1]));
+                power[2] = PID[2] + kStatic * Math.signum(PID[2]) + kV * (referenceVelocity * Math.signum(PID[2]));
+                power[3] = PID[3] + kStatic * Math.signum(PID[3]) + kV * (referenceVelocity * Math.signum(PID[3]));
 
                 wheels[0].setPower(power[0]);
                 wheels[1].setPower(power[1]);
@@ -180,8 +187,13 @@ public class PID_Practice extends LinearOpMode {
             }
 
             double kp = 1;
-            double kd = 0.0000002; // Constant of derivation
-            double ki = 0.000003;
+            double kd = 0.000075; // Constant of derivation
+            double ki = 0.000006;
+
+            double kStatic = 0;
+            double kV = 0;
+
+            double referenceVelocity = 0;
 
             double dt = 20; //Delta time = 20 ms/cycle
             double dtS = dt/1000;
@@ -192,16 +204,16 @@ public class PID_Practice extends LinearOpMode {
             double avgPreviousError = 0;
 
             double avgArea = 0;
-            double avgPreviousArea = 0;
+            double previousArea = 0;
 
             double Proportion = 0;
 
+            double PID = 0;
             double avgPower = 0;
 
             error[0] = tolerance + 1;
-            error[1] = tolerance + 1;
 
-            while (((Math.abs(error[0]) > tolerance) && (Math.abs(error[1]) > tolerance)) && opModeIsActive()) { // TODO: replace error[0] with avgError
+            while ((Math.abs(error[0]) > tolerance) && opModeIsActive()) { // TODO: replace error[0] with avgError
 
                 telemetry.addData("Path1",  "Running to %7d :%7d :%7d :%7d", newWheelTarget[0], newWheelTarget[1], newWheelTarget[2], newWheelTarget[3]);
                 telemetry.addData("Path2",  "Running at %7d :%7d :%7d :%7d", wheels[0].getCurrentPosition(), -wheels[1].getCurrentPosition(), -wheels[2].getCurrentPosition(), wheels[3].getCurrentPosition());
@@ -210,16 +222,16 @@ public class PID_Practice extends LinearOpMode {
 
                 telemetry.addData("Proportion:", Proportion);
 
-                telemetry.addData("de(t)/dt", ((avgError - avgPreviousError) / dtS));
+                telemetry.addData("Derivative:", kd * ((avgError - avgPreviousError) / dtS));
+                telemetry.addData("Integral:", ki * avgArea);
 
                 telemetry.addData("error:", avgError);
                 telemetry.addData("previous error:", avgPreviousError);
 
-                telemetry.addData("Derivative:", kd * ((avgError - avgPreviousError) / dtS));
-                telemetry.addData("Integral:", ki * avgArea);
+                telemetry.addData("de(t)/dt", ((avgError - avgPreviousError) / dtS));
 
                 telemetry.addData("∫e(t)dt:", avgArea);
-                telemetry.addData("previous ∫e(t)dt:", avgPreviousArea);
+                telemetry.addData("previous ∫e(t)dt:", previousArea);
 
                 telemetry.addData("dtS", dtS);
 
@@ -231,15 +243,17 @@ public class PID_Practice extends LinearOpMode {
                 error[2] = (int)(-distanceCM * COUNTS_PER_CM) - wheels[2].getCurrentPosition();
                 error[3] = (int)(distanceCM * COUNTS_PER_CM) - wheels[3].getCurrentPosition();
 
+                avgError = (error[0] + error[1] + error[2] + error[3])/4;
                 avgPreviousError = avgError;
-                avgError = (Math.abs(error[0]) + Math.abs(error[1]) + Math.abs(error[2]) + Math.abs(error[3])) / 4;
 
                 Proportion = Math.abs(avgError)/(int)(distanceCM * COUNTS_PER_CM);
 
-                avgPreviousArea = avgArea;
-                avgArea = avgArea * dtS + avgPreviousArea;
+                previousArea = avgArea;
+                avgArea = avgError * dtS + previousArea;
 
-                avgPower = kp * Proportion + kd * ((avgArea - avgPreviousError) / dtS) + (ki * avgArea);
+                PID = kp * Proportion + kd * ((error[0] - avgPreviousError) / dtS) + (ki * avgArea);
+
+                avgPower = PID + kStatic * Math.signum(PID) + kV * (referenceVelocity * Math.signum(PID));
 
                 wheels[0].setPower(avgPower);
                 wheels[1].setPower(-avgPower);
@@ -260,7 +274,7 @@ public class PID_Practice extends LinearOpMode {
         }
     }
 
-    public void PIDStrafe(double distanceCM, double tolerance) { // TODO: Adjust Tolerance
+    public void PIDStrafe(double distanceCM, double tolerance)  { // TODO: Adjust Tolerance
 
         int []newWheelTarget = new int[4];
 
@@ -273,10 +287,10 @@ public class PID_Practice extends LinearOpMode {
             }
 
             double kp = 1;
-            double kd = 0.00000008; // Constant of derivation
-            double ki = 0.00006;
+            double kd = 0.00015; // Constant of derivation
+            double ki = 0.00004;
 
-            double dt = 20; //Delta time = 20 ms/cycle
+            double dt = 40; //Delta time = 20 ms/cycle
             double dtS = dt/1000;
 
             double[] error = new double[4];
@@ -285,59 +299,69 @@ public class PID_Practice extends LinearOpMode {
             double avgPreviousError = 0;
 
             double avgArea = 0;
-            double avgPreviousArea = 0;
+            double previousArea = 0;
+
+            double Derivative = 0;
 
             double Proportion = 0;
 
             double avgPower = 0;
 
-            error[0] = tolerance + 1;
             error[1] = tolerance + 1;
 
-            while (((Math.abs(error[0]) > tolerance) && (Math.abs(error[1]) > tolerance)) && opModeIsActive()) { // TODO: replace error[0] with avgError
+            while ((Math.abs(error[1]) > tolerance) && opModeIsActive()) { // TODO: replace error[0] with avgError
 
-                telemetry.addData("Path1",  "Running to %7d :%7d :%7d :%7d", newWheelTarget[0], -newWheelTarget[1], newWheelTarget[2], -newWheelTarget[3]);
-                telemetry.addData("Path2",  "Running at %7d :%7d :%7d :%7d", wheels[0].getCurrentPosition(), wheels[1].getCurrentPosition(), wheels[2].getCurrentPosition(), wheels[3].getCurrentPosition());
+
+                telemetry.addData("Path1",  "Running to %7d :%7d :%7d :%7d", newWheelTarget[0], newWheelTarget[1], newWheelTarget[2], newWheelTarget[3]);
+                telemetry.addData("Path2",  "Running at %7d :%7d :%7d :%7d", wheels[0].getCurrentPosition(), -wheels[1].getCurrentPosition(), -wheels[2].getCurrentPosition(), wheels[3].getCurrentPosition());
 
                 telemetry.addData("Power: ", avgPower);
 
+                // telemetry.addData("previousArea * avgArea: ", previousArea * avgArea);
+
                 telemetry.addData("Proportion:", Proportion);
 
-                telemetry.addData("de(t)/dt", ((avgError - avgPreviousError) / dtS));
+                telemetry.addData("de(t)/dt", Derivative);
 
-                telemetry.addData("error:", avgError);
+                telemetry.addData("Derivative:", kd * ((error[1] - avgPreviousError) / dtS));
+                //telemetry.addData("Integral:", ki * avgArea);
+
+                telemetry.addData("error:", error[1]);
                 telemetry.addData("previous error:", avgPreviousError);
 
-                telemetry.addData("Derivative:", kd * ((avgError - avgPreviousError) / dtS));
-                telemetry.addData("Integral:", ki * avgArea);
+                telemetry.addData("de(t)/dt", ((error[1] - avgPreviousError) / dtS));
 
                 telemetry.addData("∫e(t)dt:", avgArea);
-                telemetry.addData("previous ∫e(t)dt:", avgPreviousArea);
+                telemetry.addData("previous ∫e(t)dt:", previousArea);
 
                 telemetry.addData("dtS", dtS);
 
                 telemetry.update();
 
+                avgPreviousError = error[1];
 
                 error[0] = (int)(distanceCM * COUNTS_PER_CM) - wheels[0].getCurrentPosition();
                 error[1] = (int)(-distanceCM * COUNTS_PER_CM) - wheels[1].getCurrentPosition();
-                error[2] = (int)(distanceCM * COUNTS_PER_CM) - wheels[2].getCurrentPosition();
-                error[3] = (int)(-distanceCM * COUNTS_PER_CM) - wheels[3].getCurrentPosition();
+                error[2] = (int)(-distanceCM * COUNTS_PER_CM) - wheels[2].getCurrentPosition();
+                error[3] = (int)(distanceCM * COUNTS_PER_CM) - wheels[3].getCurrentPosition();
 
-                avgPreviousError = avgError;
-                avgError = (Math.abs(error[0]) + Math.abs(error[1]) + Math.abs(error[2]) + Math.abs(error[3])) / 4;
+                Proportion = error[1]/Math.abs((int)(distanceCM * COUNTS_PER_CM));
 
-                Proportion = Math.abs(avgError)/(int)(distanceCM * COUNTS_PER_CM);
+                previousArea = avgArea;
+                avgArea = error[1] * dtS + previousArea;
 
-                avgPreviousArea = avgArea;
-                avgArea = avgArea * dtS + avgPreviousArea;
+                if ((error[1] * avgArea) < 0){
+                    previousArea = 0;
+                }
 
-                avgPower = kp * Proportion + kd * ((avgArea - avgPreviousError) / dtS) + (ki * avgArea);
+                avgPower = kp * Proportion + kd * Derivative + (ki * avgArea);
 
-                wheels[0].setPower(avgPower);
-                wheels[1].setPower(-avgPower);
-                wheels[2].setPower(avgPower);
-                wheels[3].setPower(-avgPower);
+                wheels[0].setPower(-avgPower);
+                wheels[1].setPower(avgPower);
+                wheels[2].setPower(-avgPower);
+                wheels[3].setPower(avgPower);
+
+                Derivative = (error[1] - avgPreviousError) / dtS;
 
                 sleep((long) dt);
             }
@@ -350,31 +374,6 @@ public class PID_Practice extends LinearOpMode {
                 wheels[i].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 wheels[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             }
-        }
-    }
-
-    public void encoderCarouselSpinner(double speed, double spinnerMM, double timeOutS){
-        int newCarouselSpinnerTarget;
-
-        if (opModeIsActive()) {
-            newCarouselSpinnerTarget = TIseBot.carouselSpinner.getCurrentPosition() + (int)(spinnerMM * COUNTS_PER_CM);
-
-            TIseBot.carouselSpinner.setTargetPosition(newCarouselSpinnerTarget);
-
-            TIseBot.carouselSpinner.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            runtime.reset();
-            TIseBot.carouselSpinner.setPower(Math.abs(speed));
-
-            while (opModeIsActive() && (runtime.seconds() < timeOutS) && (TIseBot.carouselSpinner.isBusy())) {
-                // Display it for the driver.
-                telemetry.addData("Path1",  "Running to %7d", newCarouselSpinnerTarget);
-                telemetry.addData("Path2",  "Running at %7d", TIseBot.carouselSpinner.getCurrentPosition());
-                telemetry.update();
-            }
-            TIseBot.carouselSpinner.setPower(0); // Resets encoders
-            TIseBot.carouselSpinner.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            TIseBot.carouselSpinner.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
 }
