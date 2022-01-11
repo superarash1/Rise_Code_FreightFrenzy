323fa11b3047a6450c06aaa83914044141dde8e4
superarash1 <72105024+superarash1@users.noreply.github.com>
2022-01-10 23:31:59 -0500

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -5,6 +5,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -16,17 +19,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 public class Hardware {
 
     // Define Motor Variables
-    public DcMotor frontRightMotor;
     public DcMotor frontLeftMotor;
+    public DcMotor frontRightMotor;
     public DcMotor backRightMotor;
     public DcMotor backLeftMotor;
-    public DcMotor middleLeftMotor;
-    public DcMotor middleRightMotor;
-    public DcMotor turretSpinner;
-    public DcMotor carouselSpinner;
 
-    // Define Servo Variables
-    public Servo turret;
+    public DcMotor intake;
+    public DcMotor arm1;
+    public DcMotor arm2;
+
+    public Servo cageSpin1;
+    public Servo cageSpin2;
+    public Servo gate;
+
+//  public DistanceSensor backDistance;
 
     // Define Sensor Variables
     public BNO055IMU imu;
@@ -45,34 +51,37 @@ public class Hardware {
     public void init(com.qualcomm.robotcore.hardware.HardwareMap hwMap){
         hardwareMap = hwMap;
 
-        // Find Motors in phone config
+        // Find Motors in config
+        frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeft");
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
-        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeft");
         backRightMotor = hardwareMap.get(DcMotor.class, "backRight");
         backLeftMotor = hardwareMap.get(DcMotor.class, "backLeft");
-        middleLeftMotor = hardwareMap.get(DcMotor.class, "middleLeft");
-        middleRightMotor = hardwareMap.get(DcMotor.class, "middleRight");
-        carouselSpinner = hardwareMap.get(DcMotor.class, "carouselSpinner");
-        turretSpinner = hardwareMap.get(DcMotor.class, "turretSpinner");
 
 
-        // Find Servos in phone config
-        turret = hardwareMap.get(Servo.class,"turret");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        arm1 = hardwareMap.get(DcMotor.class, "arm1");
+        arm2 = hardwareMap.get(DcMotor.class, "arm2");
 
+        cageSpin1 = hardwareMap.get(Servo.class, "cageSpin1");
+        cageSpin2 = hardwareMap.get(Servo.class, "cageSpin2");
+        gate = hardwareMap.get(Servo.class, "gate");
+//
+//        backDistance = hardwareMap.get(DistanceSensor.class, "backDistance");
 
         //Find Sensors in phone config
         imu = hardwareMap.get(BNO055IMU.class, "imu");
 
 
         // Set all motors to zero power
-        backLeftMotor.setPower(0);
-        backRightMotor.setPower(0);
         frontLeftMotor.setPower(0);
         frontRightMotor.setPower(0);
-        middleLeftMotor.setPower(0);
-        middleRightMotor.setPower(0);
-        carouselSpinner.setPower(0);
-        turretSpinner.setPower(0);
+        backRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+
+//
+        intake.setPower(0);
+        arm1.setPower(0);
+        arm2.setPower(0);
 
         // IMU Setup
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -90,30 +99,26 @@ public class Hardware {
 
         straight = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
 
-
         // Set Motor Direction
-        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
-        frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-        backRightMotor.setDirection(DcMotor.Direction.REVERSE);
-        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-        middleLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        middleRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        carouselSpinner.setDirection(DcMotor.Direction.FORWARD);
-        turretSpinner.setDirection(DcMotor.Direction.FORWARD);
+
+        intake.setDirection(DcMotor.Direction.FORWARD);
+        arm1.setDirection(DcMotor.Direction.FORWARD);
+        arm2.setDirection(DcMotor.Direction.FORWARD);
 
         // Set the Motor's Encoder Setting
         frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        middleLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        middleRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        carouselSpinner.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        turretSpinner.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-
+        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
 
